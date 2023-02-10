ac080120c6528a85549cc84faca9735a11f06be5
Meester-Moo <aidan.roraback7@gmail.com>
2023-02-09 17:11:59 -0800

    Adds framework for a second type table generation

--- sinnohdex.js
+++ sinnohdex.js
@@ -1,36 +1,58 @@
 window.onload = function() {
     
     let pokemon = [
-        ["turtwig.png", "Turtwig", "Grass", "18"],
-        ["grotle.png","Grotle", "Grass", "32"],
-        ["torterra.png","Torterra", "Grass", "Fully Evolved"]
+        ["turtwig.png", "Turtwig", "18"],
+        ["grotle.png", "Grotle", "32"],
+        ["torterra.png", "Torterra", "Fully Evolved"]
+    ]
+
+    let pokemonTypes = [
+        ["Grass"],
+        ["Grass"],
+        ["Grass", "Ground"]
     ]
 
     
-    let table = document.getElementById("poketable")
+    let table = document.getElementById("poketable");
     
 
     for (let i = 0; i < pokemon.length; i++) {
-        let tr = document.createElement("tr")
+        //Creates Row
+        let tr = document.createElement("tr");
 
-        pokemon[i]
+        //Creates table data element for pokemon image
+        let imgTd = document.createElement("td");
 
-        let imgTd = document.createElement("td")
-        let pokeImg = document.createElement("img")
-        pokeImg.src=pokemon[i][0]
-        let nameTd = document.createElement("td")
-        nameTd.innerText = pokemon[i][1]
-        let typeTd = document.createElement("td")
-        typeTd.innerText = pokemon[i][2]
-        let evolveLvlTd = document.createElement("td")
-        evolveLvlTd.innerText = pokemon[i][3]
+        //Creates img element for pokemon image
+        let pokeImg = document.createElement("img");
+        pokeImg.src=pokemon[i][0];
 
-        tr.appendChild(imgTd)
-        tr.appendChild(pokeImg)
-        tr.appendChild(nameTd)
-        tr.appendChild(typeTd)
-        tr.appendChild(evolveLvlTd)
-        table.appendChild(tr)
+        //Creates table data element for pokemon name
+        let nameTd = document.createElement("td");
+        nameTd.innerText = pokemon[i][1];
+        
+        //Creates table data element for pokemon type1
+        let type1Td = document.createElement("td");
+        type1Td.innerText = pokemonTypes[i][0];
+
+        //Creates table data element for pokemon type2
+        let type2Td = document.createElement("td");
+        type2Td.innerText= pokemonTypes[i][1];
+
+        //Creates table data element for pokemon level
+        let evolveLvlTd = document.createElement("td");
+        evolveLvlTd.innerText = pokemon[i][2];
+
+        tr.appendChild(imgTd);
+        tr.appendChild(pokeImg);
+        tr.appendChild(nameTd);
+        tr.appendChild(type1Td);
+        tr.appendChild(type2Td);
+        tr.appendChild(evolveLvlTd);
+        table.appendChild(tr);
     }
 
 }
+
+
+//TODO: Add table headings (IMG/NAME/TYPE/EVOLVE etc)
