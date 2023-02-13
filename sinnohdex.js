0537e364778675d2214d93da3042641a07e38ba4
Meester-Moo <aidan.roraback7@gmail.com>
2023-02-13 12:18:32 -0800

    Adds table headers and fixes extra first td of each row

--- sinnohdex.js
+++ sinnohdex.js
@@ -1,27 +1,58 @@
 window.onload = function() {
     
     let pokemon = [
-        ["turtwig.png", "Turtwig", "18"],
-        ["grotle.png", "Grotle", "32"],
+        ["turtwig.png", "Turtwig", "Level up (lvl. 18)"],
+        ["grotle.png", "Grotle", "Level up (lvl. 32)"],
         ["torterra.png", "Torterra", "Fully Evolved"]
     ]
 
     let pokemonTypes = [
-        ["Grass"],
-        ["Grass"],
+        ["Grass", ""],
+        ["Grass", ""],
         ["Grass", "Ground"]
     ]
 
-    
+    //Creates table element and associates it with the poketable table in the HTML
     let table = document.getElementById("poketable");
+
+    //Creates the first row of the table (the heading row)
+    let trHeading = document.createElement("tr");
+    table.appendChild(trHeading);
+    
+    //Creates the heading table data element for the images column
+    let imgHeadingTd = document.createElement("td");
+    imgHeadingTd.innerText = "Official Art";
+    trHeading.appendChild(imgHeadingTd);
+
+    //Creates the heading table data element for the name column
+    let nameHeadingTd = document.createElement("td");
+    nameHeadingTd.innerText = "Name";
+    trHeading.appendChild(nameHeadingTd);
+
+    //Creates the heading table data element for the type1 column
+    let type1HeadingTd = document.createElement("td");
+    type1HeadingTd.innerText = "Type 1";
+    trHeading.appendChild(type1HeadingTd);
+
+    //Creates the heading table data element for the type2 column
+    let type2HeadingTd = document.createElement("td");
+    type2HeadingTd.innerText = "Type 2";
+    trHeading.appendChild(type2HeadingTd);
+
+    //Creates the heading table data element for the Evolution Method column
+    let evolveMethodHeadingTd = document.createElement("td");
+    evolveMethodHeadingTd.innerText = "Evolution Method";
+    trHeading.appendChild(evolveMethodHeadingTd);
     
 
+    //For Loop that generates the actual table contents
+    // 1) Creates the row for that pokemon
+    // 2) Creates the elements and fills in the data for each element for that pokemon 
+    // 3) Appends the HTML elements into the row for that pokemon
+    // 4) Appends the row for that pokemon into the table 
     for (let i = 0; i < pokemon.length; i++) {
         //Creates Row
         let tr = document.createElement("tr");
-
-        //Creates table data element for pokemon image
-        let imgTd = document.createElement("td");
 
         //Creates img element for pokemon image
         let pokeImg = document.createElement("img");
@@ -40,19 +71,20 @@ window.onload = function() {
         type2Td.innerText= pokemonTypes[i][1];
 
         //Creates table data element for pokemon level
-        let evolveLvlTd = document.createElement("td");
-        evolveLvlTd.innerText = pokemon[i][2];
+        let evolveMethodTd = document.createElement("td");
+        evolveMethodTd.innerText = pokemon[i][2];
 
-        tr.appendChild(imgTd);
+        // tr.appendChild(imgTd);
         tr.appendChild(pokeImg);
         tr.appendChild(nameTd);
         tr.appendChild(type1Td);
         tr.appendChild(type2Td);
-        tr.appendChild(evolveLvlTd);
+        tr.appendChild(evolveMethodTd);
         table.appendChild(tr);
     }
 
 }
 
 
-//TODO: Add table headings (IMG/NAME/TYPE/EVOLVE etc)
+//TODO: Add Chimchar and Piplup line
+//TODO: Figure out how to add CSS to specific types
